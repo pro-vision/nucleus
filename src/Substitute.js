1b7e4b1f392aadac05caafdbec102353018d6ee1
Fritz Stelluto <fstelluto@pro-vision.de>
2017-08-24 12:02:03 +0200

    IFRAME | added a new iframe substitution

--- src/Substitute.js
+++ src/Substitute.js
@@ -145,6 +145,26 @@ Substitute.methods.template = function ( selector = '') {
     Verbose.warn('unknown_template', [filePath, selector, this.templatePath]);
     return '';
   }
-}
+};
+
+Substitute.methods.iframe = function ( url = '', width, height) {
+  url = url.trim();
+  // not using default parameters because calling substitution
+  // @{iframe:url::111} will pass an empty string to width,
+  // which needs one of the below anyway - therefore let's just
+  // do them all here
+  width = width || '100%';
+  height = height || '100vh';
+  if (url) {
+    var id = `ifrm_${String(Math.random()).substr(3)}`;
+    return `
+    <iframe class="nuclueus-iframe" id="${id}" style="width: ${width}; height: ${height};" src="${url}"></iframe>
+    `;
+  }
+  else {
+    Verbose.warn('no_iframe_url', []);
+    return ''
+  }
+};
 
 module.exports = Substitute;
